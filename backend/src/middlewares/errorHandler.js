ff6bd6968d7f1511fe5bbd2e57b9b8e29b20ec8a
amine-achtouk <achtoukmohamedamine1@gmail.com>
2026-02-25 22:07:30 +0100

    Organised gitgnore

--- backend/src/middlewares/errorHandler.js
+++ backend/src/middlewares/errorHandler.js
@@ -1,5 +1,4 @@
 const errorHandler = (err, req, res, next) => {
-  console.log(err)
   const statusCode = err.statusCode || 500;
   const message = err.isOperational ? err.message : 'Something went wrong';
 
